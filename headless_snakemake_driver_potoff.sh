8811b5d909dedda14a8bcb489032835a34e16f35
go2432 <go2432@wayne.edu>
2023-11-19 04:36:25 -0600

    add runner for potoff

--- headless_snakemake_driver_potoff.sh
+++ headless_snakemake_driver_potoff.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 
-#SBATCH --job-name snake
+#SBATCH --job-name nxfl
 
 #SBATCH -N 1
 
 #SBATCH -n 1 
-
 #SBATCH --mem=10G
-#SBATCH --gpus-per-node=1
-#SBATCH --ntasks-per-gpu=1
-#SBATCH --nodelist=ressrv4ai8111,ressrv6ai8111
-
 #SBATCH --mail-type=ALL
-
 #SBATCH --mail-user=go2432@wayne.edu
-
 #SBATCH -o output_%j.out
-
 #SBATCH -e errors_%j.err
-
 #SBATCH -t 7-0:0:0
-eval "$(conda shell.bash hook)"
-source /home6/go2432/Wolf_GOMC/workflow/mambaforge/etc/profile.d/conda.sh
-conda activate snakemake-minimal
-snakemake --unlock
-#cd /home/scratch/Wolf_GOMC/workflow
-#snakemake -c 1 --use-conda
-snakemake -c 1 --use-conda --rerun-triggers mtime  --rerun-incomplete
-echo $HOSTNAME
+source "${HOME}/mambaforge/etc/profile.d/mamba.sh"
+# Contains nextflow and singularity
+source activate nextflow
+export NXF_EXECUTOR=slurm
+export NXF_OPTS="-Xms2G -Xmx8G" 
+mkdir -p ${HOME}/singularity_cache
+export NXF_SINGULARITY_CACHEDIR=${HOME}/singularity_cache
+mkdir -p ${HOME}/xdr
+export XDG_RUNTIME_DIR=${HOME}/xdr
+nextflow run -profile potoff . --param_name nextflow.config
